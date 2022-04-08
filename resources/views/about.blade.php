13378ab2d993cad758de71897b3cc1ea5e9b069f
GioTsatskrialashvili <giocackri1@gmail.com>
2022-04-08 21:31:50 +0400

    first commit

---
+++ resources/views/about.blade.php
@@ -0,0 +1,9 @@
+@extends ('layout')
+@section ('content')
+        <main>
+            About page
+        </main>
+@endsection
+  @section ('test-footer')
+ <p> this is about page footer</p>
+  @endsection
---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,10 @@
+@extends ('layout')
+@section ('content')
+        <main>
+            main page
+        </main>
+@endsection
+@section ('test-footer')
+ <p> this is main page footer</p>
+  @endsection
+  
