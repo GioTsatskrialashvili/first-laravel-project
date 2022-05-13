e4bba0a1fc8d1db953726712d5a3760aa8cc0aab
GioTsatskrialashvili <giocackri1@gmail.com>
2022-05-13 23:00:52 +0400

    edit file

--- app/Http/Controllers/Admin/AboutController.php
+++ app/Http/Controllers/Admin/AboutController.php
@@ -4,11 +4,56 @@ namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use App\Models\About;
 
 class AboutController extends Controller
 {
+
+      /**
+     * Create a new controller instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        $this->middleware('auth');
+    }
+
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
     public function index()
     {
-        return view('admin.about.index');
+        $about = About::first();
+
+        return view('admin.about.index')->with('data', ['about' => $about]);
     }
+
+  
+
+  
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request)
+    {
+        $title = $request->title;
+        $short_text = $request->short_text;
+        $text = $request->text;
+
+        About::first()->update([
+            'title' => $title,
+            'short_text' => $short_text,
+            'text' => $text
+        ]);
+
+        return redirect()->route('admin.about');
+    }
+
 }
--- resources/views/admin/contact/index.blade.php
+++ resources/views/admin/contact/index.blade.php
@@ -1,4 +1,4 @@
-@extends('admin.layout')
+@extends('admin.app')
 
 @section('content')
 <div class="container">
